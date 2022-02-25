fb3df3cfa46316460d4db6c5fe4aa575ecdf607f
Edgar <esedsa75@gmail.com>
2022-02-25 17:04:10 +0100

    Swipeable app

---
+++ www/js/init.js
@@ -0,0 +1,18 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+    //$('.parallax').parallax();
+    $('.tabs').tabs({"swipeable": true});
+    
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+document.addEventListener('deviceready', onDeviceReady, false);
+ 
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+ 
+    console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
+    //document.getElementById('deviceready').classList.add('ready');
+}
